c1f69788899b8373552d08131743540ff2177660
Erin Claudio <eclaudio@gmail.com>
2019-09-12 14:33:22 -0400

    second attempt - file naming error

--- lib/card.rb
+++ lib/card.rb
@@ -1,15 +1,25 @@
 require './lib/turn'
+require './lib/deck'
 
 
 class Card
-  attr_reader :question,
-              :answer,
-              :category
 
   def initialize(question, answer, category)
     @question = question
     @answer   = answer
     @category = category
+  end
+
+  def question
+    @question
+  end
+
+  def answer
+    @answer
+  end
+
+  def category
+    @category
   end
 
 end
